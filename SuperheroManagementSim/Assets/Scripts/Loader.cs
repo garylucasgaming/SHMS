6bb0cbf19b8373d3c018c4776aa178965f6dfee4
garylucasgaming <paradoxsc2@gmail.com>
2018-01-17 19:39:25 -0500

    first real commit
    addded manager classes

---
+++ SuperheroManagementSim/Assets/Scripts/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    //prefabs to instatiate
+    public GameObject gameManager; 
+    public GameObject soundManager;
+
+	
+	void Awake () {
+
+        //check if GameManager has already been assigned to Gamemanager.instance, if null instantiate
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+
+        if (SoundManager.instance == null)
+            Instantiate(soundManager);
+
+
+    }
+	
+}
---
+++ SuperheroManagementSim/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour{
+
+    public static SoundManager instance = null;
+
+    void Awake()
+    {
+        //check if instance exists
+        if (instance == null)
+            //if not, set instance to this
+            instance = this;
+        //if instance exists, and its not this
+        else if (instance != this)
+            //destroy this, enforcing singleton
+            Destroy(gameObject);
+
+        //set this to not be destroyed when reloading
+        DontDestroyOnLoad(gameObject);
+
+        
+
+    }
+
+}
